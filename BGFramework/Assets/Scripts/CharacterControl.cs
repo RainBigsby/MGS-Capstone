73e66c56c39dec94244b113acbd614e4e5917a33
Lenin Valdivia <59747577+Oqqay@users.noreply.github.com>
2024-03-05 18:08:16 -0700

    imported some packaged components

---
+++ BGFramework/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    [SerializeField] private Character character;
+    private void Update()
+    {
+        #region WALKING
+        //handle player right input
+        if (Input.GetAxisRaw("Horizontal") == 1f)
+        {
+            character.moveCharacterRight();
+        }
+
+        //handle player left input
+        if (Input.GetAxisRaw("Horizontal") == -1f)
+        {
+            character.moveCharacterLeft();
+        }
+
+        //handle player up input
+        if (Input.GetAxisRaw("Vertical") == 1f)
+        {
+            character.moveCharacterUp();
+        }
+
+        //handle player down input
+        if (Input.GetAxisRaw("Vertical") == -1f)
+        {
+            character.moveCharacterDown();
+        }
+
+        //reseting character speed when no input is decteced
+        if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            character.resetSpeedHorizontal();
+        }
+
+        if (Input.GetAxisRaw("Vertical") == 0)
+        {
+            character.resetSpeedVertical();
+        }
+        #endregion
+
+        #region JUMPING
+        character.checkGrounded();
+        if (Input.GetButtonDown("Jump"))
+        {
+            character.characterJump();
+        }
+        
+        if(Input.GetAxisRaw("Jump") == 0)
+        {
+            character.resetJump();
+        }
+        #endregion
+    }
+}
---
+++ BGFramework/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    // where the player starts in the game i.e checkpoint
+    private Vector3 respawnPoint;
+    public GameObject fallDetector;
+
+    // change scene name in inspector
+    [SerializeField] public string sceneName;
+
+    void Start()
+    {
+        // get the position of the player
+        respawnPoint = transform.position;
+    }
+
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // when player enters goal
+        if (collision.name == "Player")
+        {
+            Debug.Log("Player Hit Goal");
+
+            // go to next level
+            SceneManager.LoadScene(sceneName);
+        }
+
+        // if player falls off platforms
+        if (collision.name == "FallDetector") 
+        {
+            transform.position = respawnPoint;
+        }
+    }
+
+    // if player falls off the platforms
+}
